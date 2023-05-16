99f989ae872fa3156d16ee8a68ec39416e2fc400
arthurssn <arthurnovaes55@gmail.com>
2023-05-16 13:22:41 -0300

    feat: entidade de cidades

---
+++ src/city/entities/city.entity.ts
@@ -0,0 +1,25 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'city' })
+export class CityEntity {
+  @PrimaryGeneratedColumn('rowid')
+  id: number;
+
+  @Column({ name: 'state_id', nullable: false })
+  userId: number;
+
+  @Column({ name: 'name' })
+  name: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  created_at: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updated_at: Date;
+}
