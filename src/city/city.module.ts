812023c53f6dc1b212ae0dd65cf917d29e5f943d
arthurssn <arthurnovaes55@gmail.com>
2023-05-17 20:36:37 -0300

    feat: modulo de cidades

--- src/city/city.module.ts
+++ src/city/city.module.ts
@@ -1,4 +1,24 @@
 import { Module } from '@nestjs/common';
+import { CityController } from './city.controller';
+import { CityService } from './city.service';
+import { ICityService } from './interfaces/city-service.interface';
+import { ICityRepository } from './interfaces/city-repository.interface';
+import { CityRepository } from './city.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CityEntity } from './entities/city.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([CityEntity])],
+  controllers: [CityController],
+  providers: [
+    {
+      provide: ICityService,
+      useClass: CityService,
+    },
+    {
+      provide: ICityRepository,
+      useClass: CityRepository,
+    },
+  ],
+})
 export class CityModule {}
