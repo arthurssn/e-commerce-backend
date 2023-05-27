20bb6a64bc124bef1cd68fb0488d6cbd55210c48
arthurssn <arthurnovaes55@gmail.com>
2023-05-27 20:31:43 -0300

    fix: deixa rota de login publica

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  ValidationPipe,
+  SetMetadata,
+} from '@nestjs/common';
 import { LoginDto } from './dtos/login.dto';
 import { returnUserDto } from 'src/user/dtos/returnUser.dto';
 import { IAuth } from './interfaces/auth.interface';
 import { IAuthService } from './interfaces/auth-service.interface';
 
+const Public = () => SetMetadata('isPublic', true);
 @Controller('auth')
 export class AuthController implements IAuth {
   constructor(private readonly authService: IAuthService) {}
 
+  @Public()
   @Post()
   async login(
     @Body(new ValidationPipe()) loginDto: LoginDto,
