ddfea23b83578f79595a13d59d697b66497bd0cf
arthurssn <arthurnovaes55@gmail.com>
2023-05-27 19:10:44 -0300

    refactor: add state endpoints como publicos

--- src/state/state.controller.ts
+++ src/state/state.controller.ts
@@ -2,11 +2,13 @@ import { Controller, Get } from '@nestjs/common';
 import { IStateCRUD } from './interfaces/state-crud.interface';
 import { IStateService } from './interfaces/state-service.interface';
 import { ReturnStateDto } from './dtos/return-state.dto';
+import { Public } from 'src/auth/auth.module';
 
 @Controller('state')
 export class StateController implements IStateCRUD {
   constructor(private readonly stateService: IStateService) {}
 
+  @Public()
   @Get()
   async findAll(): Promise<ReturnStateDto[]> {
     return await this.stateService.findAll();
