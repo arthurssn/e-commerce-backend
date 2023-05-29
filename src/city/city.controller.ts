4a4513b50e6519633e9dbc8acf9febb0a43d1f6b
arthurssn <arthurnovaes55@gmail.com>
2023-05-29 12:21:39 -0300

    fix: add param na rota para get by id

--- src/city/city.controller.ts
+++ src/city/city.controller.ts
@@ -1,14 +1,14 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Param, Controller, Get, Query } from '@nestjs/common';
 import { ICityCrud } from './interfaces/city-crud.interface';
 import { ICityService } from './interfaces/city-service.interface';
 import { ReturnCityDto } from './dtos/return-city-dto';
 import { Public } from 'src/auth/auth.module';
 
+@Public()
 @Controller('city')
 export class CityController implements ICityCrud {
   constructor(private readonly cityService: ICityService) {}
 
-  @Public()
   @Get()
   async findAll(@Query('state') stateId?: number): Promise<ReturnCityDto[]> {
     if (stateId) {
@@ -17,7 +17,8 @@ export class CityController implements ICityCrud {
     return await this.cityService.findAll();
   }
 
-  async findById(cityId: number): Promise<ReturnCityDto> {
+  @Get('/:cityId')
+  async findById(@Param('cityId') cityId: number): Promise<ReturnCityDto> {
     return await this.cityService.findById(cityId);
   }
 }
