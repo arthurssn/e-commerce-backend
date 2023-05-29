729075d4aa7b2e62ed489e8f49fc9e4feb4b74b5
arthurssn <arthurnovaes55@gmail.com>
2023-05-29 13:54:46 -0300

    refactor: utiliza decorator UserId para receber id do usuario

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -3,16 +3,17 @@ import { IAddressCrud } from './interfaces/address-crud.interface';
 import { CreateAddressDto } from './dtos/createAddress.dto';
 import { AddressEntity } from './entities/address.entity';
 import { IAddressService } from './interfaces/address-service.interface';
+import { UserId } from 'src/decorators/user-id.decorator';
 
 @Controller('address')
 export class AddressController implements IAddressCrud {
   constructor(private readonly addressService: IAddressService) {}
 
-  @Post('/:userId')
+  @Post()
   async create(
     @Body(new ValidationPipe())
     createAddressDto: CreateAddressDto,
-    @Param('userId')
+    @UserId()
     userId: number,
   ): Promise<AddressEntity> {
     return this.addressService.create(createAddressDto, userId);
